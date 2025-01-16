d69c6009989479380e303357a8da6fcad58d87c2
Joona Perasto <jonspe.pe@gmail.com>
2025-01-16 16:21:45 +0200

    Add padding for smaller resolutions, reduce fade transition to 0.4s

--- src/app/components/fadediv.js
+++ src/app/components/fadediv.js
@@ -4,7 +4,7 @@ import { motion } from "motion/react"
 
 export default function FadeDiv({ children }) {
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.7 }}>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.4 }}>
       {children}
     </motion.div>
   )
