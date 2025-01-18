02ff5c69e5484f4b833ec75748a20514d15717f6
Joona Perasto <jonspe.pe@gmail.com>
2025-01-18 15:35:05 +0200

    Fix mobile whitespace

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,11 @@ export const metadata = {
   title: "Staattinen Next.js Blogi",
 }
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
